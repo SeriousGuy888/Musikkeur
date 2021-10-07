86217b39fdb4291845a3a0ef40fe436bd69b6274
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-10-06 21:24:59 -0400

    Initial commit

---
+++ src/main/java/io/github/seriousguy888/musikkeur/Musikkeur.java
@@ -0,0 +1,17 @@
+package io.github.seriousguy888.musikkeur;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Musikkeur extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
