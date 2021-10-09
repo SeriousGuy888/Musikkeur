10d8d683847f273f0c6c19c760611d0f8562b43c
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2021-10-09 14:26:44 -0400

    added canvas gui library

--- src/main/java/io/github/seriousguy888/musikkeur/Musikkeur.java
+++ src/main/java/io/github/seriousguy888/musikkeur/Musikkeur.java
@@ -1,13 +1,19 @@
 package io.github.seriousguy888.musikkeur;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.ipvp.canvas.MenuFunctionListener;
 
 public final class Musikkeur extends JavaPlugin {
-
   @Override
   public void onEnable() {
-    // Plugin startup logic
+    registerListeners();
+  }
 
+  private void registerListeners() {
+    PluginManager pluginManager = Bukkit.getPluginManager();
+    pluginManager.registerEvents(new MenuFunctionListener(), this);
   }
 
   @Override
